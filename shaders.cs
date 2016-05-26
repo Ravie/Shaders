f1d0e673db2af837555c46aceb88b1778e621014
Kirill <ravijkee@yandex.ru>
2016-05-26 04:32:49 +0300

    Remove unused classes

--- shaders.cs
+++ shaders.cs
@@ -2,11 +2,6 @@
 using OpenTK.Graphics.OpenGL;
 using System;
 using System.IO;
-using System.Drawing;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Shaders
 {
